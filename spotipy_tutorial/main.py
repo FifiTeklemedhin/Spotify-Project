afa0f9d89cf16bbcf7e4ef21ec7617385d857d2a
Fikir Teklemedhin <fi.leul3562@gmail.com>
2022-11-22 17:04:20 -0500

    initial commit

--- spotipy_tutorial/main.py
+++ spotipy_tutorial/main.py
@@ -25,8 +25,9 @@ def writeTopTracksHTML():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id= credentials.SPOTIPY_CLIENT_ID, client_secret= credentials.SPOTIPY_CLIENT_SECRET, redirect_uri= credentials.SPOTIPY_REDIRECT_URI, scope=scope))
 
     # requests client's top tracks and lists them out w/ link
-    # can change time_range to short_term (past 4 weeks) or middle_term (default, past 6 months)
-    results = sp.current_user_top_tracks(time_range= "long_term")
+    # can change time_range param to short_term (past 4 weeks) or middle_term (default, past 6 months)
+    # ex: results = sp.current_user_top_tracks(time_range= "short_term")
+    results = sp.current_user_top_tracks(time_range= "short_term")
 
     ##print(results['items'][0]['name'] + " : " + results['items'][0]['external_urls']['spotify'])
     new_line = ""
